d7da596c72bc8f436fe5798880f0394a592cfe10
Kazuho Kambara <>
2020-04-13 22:29:16 +0900

    Add STI to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  validates :name,  presence: true
+  validates :email, presence: true
+  validates :type,  presence: true
+
   def show_name
-    "#{self.name.present? ? self.name : self.email }（#{self.teacher ? "教師" : "学生"}）"
+    self.name.present? ? self.name : self.email
   end
 end
